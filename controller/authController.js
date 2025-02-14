c4d1baabe097eb2882101866c77b45cbd32a2d75
princedev3 <marvinprince232@gmail.com>
2025-02-15 01:22:31 +0200

    cons

--- controller/authController.js
+++ controller/authController.js
@@ -81,7 +81,7 @@ export const login = async (req, res) => {
     }
     const { accessToken, refreshToken } = await generateToken(user?.id, email);
     const ress = await setToken(res, accessToken, refreshToken);
-
+    console.log("Cookie Set:", res.getHeaders()["set-cookie"]);
     const { password: userPassword, ...userProperties } = user;
     res
       .status(200)
