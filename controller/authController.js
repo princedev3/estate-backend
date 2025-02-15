63676884035c0a4e97b440b67beb56cbf4e7f4a2
princedev3 <marvinprince232@gmail.com>
2025-02-16 00:22:54 +0200

    laxs

--- controller/authController.js
+++ controller/authController.js
@@ -51,14 +51,14 @@ export const register = async (req, res) => {
 const setToken = async (response, accessToken, refreshToken) => {
   response.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
     path: "/",
     maxAge: 60 * 60 * 24 * 7 * 1000,
   });
   response.cookie("accessToken", accessToken, {
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
     path: "/",
     maxAge: 60 * 60 * 24 * 7 * 1000,
