d90e6308a5ac11f51acd7861f1be244fb55bdf28
princedev3 <marvinprince232@gmail.com>
2025-02-15 10:11:14 +0200

    same-site

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://estate-frontend-armmeza6l-princedev3s-projects.vercel.app",
+    origin: "http://localhost:3000",
     credentials: true,
     method: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
