c97f04543131a7596809436f468d871080ae4a3c
princedev3 <marvinprince232@gmail.com>
2025-03-09 13:02:06 +0200

    strict

--- controller/authController.js
+++ controller/authController.js
@@ -58,8 +58,8 @@ const setToken = async (response, accessToken, refreshToken) => {
   });
   response.cookie("accessToken", accessToken, {
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    secure: true,
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
